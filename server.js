d9efacd7ee3b51ac09d425364649225befdcebd1
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-09-18 18:14:09 -0300

    :rocket: Port override via environment

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 
 app.use(express.static('.'));
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`App listening on port ${PORT}!`);
 });
